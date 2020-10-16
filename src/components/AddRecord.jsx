ffc17501fc12b4ae714a129d1724ecd879aa06e2
Nish <nish.gnanalingam@nationwide.co.uk>
2020-10-16 14:02:13 +0100

    AddModal toggle

--- src/components/AddRecord.jsx
+++ src/components/AddRecord.jsx
@@ -7,14 +7,21 @@ const AddRecord = () => {
 
     const dispatch = useDispatch();
 
+    const [showModal, setShowModal] = useState(false)
     const [ninumber, setNinumber] = useState('');
     const [fullname, setFullname] = useState('');
     const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
     const [department, setDepartment] = useState('');
 
+    const modalHandler = () => {
+        setShowModal(!showModal)
+    }
+
     const addHandler = (e) => {
+        
         e.preventDefault();
+        
         dispatch(addRecord({
             ninumber,
             fullname,
@@ -22,21 +29,23 @@ const AddRecord = () => {
             address,
             department,
         }))
+
+        modalHandler();
     }
 
     return (
         <div>
-        <div>
+        <div onClick = {modalHandler}>
             Add
         </div>
-        <form className = 'edit-modal' onSubmit = {addHandler}> 
+        {showModal && <form className = 'edit-modal' onSubmit = {addHandler}> 
         NI Number: <input type = 'text'  id='ninumber' onChange={event => setNinumber(event.target.value)}/>
         Full Name: <input type = 'text'  id='fullname' onChange={event => setFullname(event.target.value)}/>                 
         Phone No: <input type = 'text'  id='phone' onChange={event => setPhone(event.target.value)}/>                 
         Address:  <input type = 'text'  id='address' onChange={event => setAddress(event.target.value)}/>                 
         Department:  <input type = 'text'  id='department' onChange={event => setDepartment(event.target.value)}/> 
         <input type = 'submit' value = 'Submit' />                
-        </form>
+        </form>}
 
         </div>
     )
