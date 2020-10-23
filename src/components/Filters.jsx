79f443339c589215ed0deb21c62d89bc300626f1
Nish <nish.gnanalingam@nationwide.co.uk>
2020-10-23 21:44:26 +0100

    Added logic to make filter buttons update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,6 +6,9 @@ const Filters = () => {
 
     const dispatch = useDispatch();
     const filters = useSelector(state => state.filters);
+    const records = useSelector(state => state.records);
+
+    const departments = new Set(records.map(x => x.department));
 
     const filterHandler = (filter) => {
         if (filters.indexOf(filter) === -1){
@@ -18,10 +21,10 @@ const Filters = () => {
     return (
         <div className = 'filters-container'>
             Filters:
-            <button onClick={() => filterHandler("Sales")} className = {filters.indexOf("Sales") === -1 ? 'inactive-filter filter' : 'active-filter filter'}> Sales </button>
-            <button onClick={() => filterHandler("IT")} className = {filters.indexOf("IT") === -1 ? 'inactive-filter filter' : 'active-filter filter'}> IT </button>
-            <button onClick={() => filterHandler("HR")} className = {filters.indexOf("HR") === -1 ? 'inactive-filter filter' : 'active-filter filter'}> HR </button>
+
+            {[...departments].map(dept  => <button className = {filters.indexOf("Sales") === -1 ? 'inactive-filter filter' : 'active-filter filter'} onClick={() => filterHandler(dept) }> {dept} </button>)}
             <button onClick ={() => dispatch(clearFilter())}> Clear </button>
+            <button onClick= {() => console.log(departments)}> kundi</button>
         </div>
     )
 
